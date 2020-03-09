3cecc1bb95431ab012f6ce316614dcfacc20601a
chang70480 <35148988+chang70480@users.noreply.github.com>
2020-03-09 23:53:20 +0800

    Update testpackage.R

--- inst/pk_test/testpackage.R
+++ inst/pk_test/testpackage.R
@@ -10,13 +10,14 @@ library(YCfun)
 ##
 detach("package:YCfun", unload=TRUE)
 remove.packages("YCfun")
-install.packages("~/Google 雲端硬碟/社會系/R/YCfun_0.1.1.tar.gz", repos = NULL, type = "source")
+#install.packages("~/Google 雲端硬碟/社會系/R/YCfun_0.1.1.tar.gz", repos = NULL, type = "source")
 
 
 
 library(devtools)
-install_github("chang70480/YCfun")
+install_github("chang70480/YCfun",)
 ?install_github
 library(YCfun)
 
 rec_new(1:4,"1,2=0;3,4=1")
+rec.new(1:4,"1,2=0;3,4=1")
