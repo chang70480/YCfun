eb1d71830dc395d8018dc6ca5179f7a5e33f077e
chang70480 <35148988+chang70480@users.noreply.github.com>
2020-03-14 22:19:10 +0800

    chang70480

--- R/3_recode.R
+++ R/3_recode.R
@@ -155,13 +155,19 @@ re.table <- function(vec=vec){
 #' recode NA
 #'
 #' recode NA into specific value
+#'
 #' @param vec vector
 #' @param to recode into ?
+#' @param Inf_include recode including Inf?
 #'
 #' @export
 #' @return vector
-nato <- function(vec,to){
-	vec[is.na(vec)] <- to
+nato <- function(vec,to,Inf_include=T){
+	if(Inf_include){
+		vec[is.na(vec)|is.infinite(vec)] <- to
+	}else{
+		vec[is.na(vec)] <- to
+	}
 	return(vec)
 }
 
