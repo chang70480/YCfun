ffef992c39b50419b79fa97a688d08bd56012338
chang70480 <35148988+chang70480@users.noreply.github.com>
2020-09-03 17:06:10 +0800

    chang

--- R/3_recode.R
+++ R/3_recode.R
@@ -205,3 +205,25 @@ change_posi <- function(recordtab,old=NULL,quickNA=T){
 	}
 	return(out)
 }
+
+
+
+#' Title new version data names
+#'
+#' @param path file path ,without filename
+#' @param filename filename
+#' @param nversion new version
+#' @param update
+#'
+#' @return
+#' @export
+#'
+#' @examples
+newversion <- function(path,filename,nversion=F,update=T){
+	namelist_ <- grep(filename,dir(path),value = T)
+	version <- str_sub(namelist_,-9,-7)
+	version <- str_split(version,"\\.") %>% unlist() %>% as.numeric()
+	if(nversion) version[1] <- version[1]+1
+	if(update)   version[2] <- version[2]+1
+	paste0(path,"/",filename,"_",paste(version,collapse = "."),".RData")
+}
