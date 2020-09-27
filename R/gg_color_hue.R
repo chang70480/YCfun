fd7726efa83231f375f528553fa5f6075efd4687
chang70480 <35148988+chang70480@users.noreply.github.com>
2020-09-27 22:58:18 +0800

    chang70480

--- R/gg_color_hue.R
+++ R/gg_color_hue.R
@@ -3,13 +3,22 @@
 #' This function can generate html color codes from ggplot color styles.
 #'
 #' @param n How many color hueps do you want?
-#'
+#' @param change
 #' @return thml  color codes.
 #' @export
 #' @examples
 #' gg_color_hue(5)
 #' #F8766D #A3A500 #00BF7D #00B0F6 #E76BF3
-gg_color_hue <- function(n) {
-	hues = seq(15, 375, length = n + 1)
-	hcl(h = hues, l = 65, c = 100)[1:n]
+gg_color_hue <- function(n,change=list()){
+	if(!is.numeric(n)) n <- length(unique(n))
+	if(length(names(change))==0 & length(change)>0){
+		names(change) <- as.character((n-length(change)+1):n)
+	}
+	len <- length(change)
+	n2 <- n-len
+	col <- as.character(1:n)
+
+	col[!((1:n) %in% as.numeric(names(change)))] <- hcl(h = seq(15, 375, length = n2 + 1), l = 65, c = 100)[1:n2]
+	col[as.numeric(names(change))] <- unlist(change)
+	col
 }
