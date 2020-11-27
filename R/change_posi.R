64100030f3aa101b9244484689f9be231f1f3c7e
chang70480 <35148988+chang70480@users.noreply.github.com>
2020-11-27 20:35:51 +0800

    Update change_posi.R

--- R/change_posi.R
+++ R/change_posi.R
@@ -7,14 +7,25 @@
 #'
 #' @return recoded vector
 #' @export
-change_posi <- function(recordtab,old=NULL,quickNA=T){
-	if(quickNA){
+change_posi <- function(recordtab,old=NULL,quickNA=T,copy=F){
+	if(quickNA & !copy){
 		old[!(old %in% recordtab[[1]])] <- NA
+	}else if(copy){
+		copylist <- unique(old[! old %in% recordtab[[1]]])
 	}
 	recordtab[[1]] <- as.character(recordtab[[1]])
 	NAr <- data_frame(V1="NA",V2=NA)
 	names(NAr) <- names(recordtab)
-	recordtab <- bind_rows(recordtab,NAr)
+	if(exists("copylist")){
+		copyr <- data_frame(V1=copylist,V2=copylist)
+		names(copyr) <- names(recordtab)
+		recordtab <- bind_rows(recordtab,copyr,NAr)
+	}else{
+		recordtab <- bind_rows(recordtab,NAr)
+	}
+
+
+
 
 	new <- recordtab[[2]]
 	names(new) <- recordtab[[1]]
@@ -28,3 +39,5 @@ change_posi <- function(recordtab,old=NULL,quickNA=T){
 	}
 	return(out)
 }
+
+
