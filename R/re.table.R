8e0e4b2b32463def0a5b25752aaecda3559cdabe
chang70480 <35148988+chang70480@users.noreply.github.com>
2020-11-27 11:48:57 +0800

    chang70480

--- R/re.table.R
+++ R/re.table.R
@@ -2,16 +2,26 @@
 #'
 #' recode data attribete's table
 #' @param vec vector or data_frame
+#' @param len lenght of table summary
 #'
 #' @return vector or data_frame
 #' @export re.table
-re.table <- function(vec=vec){
+re.table <- function(vec=vec,len=25){
+	ll <- function(vec2,len){
+		lens <- ifelse(length(vec2)<len,length(vec2),len)
+		if(lens!=0) return(vec2[1:lens])
+			else return(table(as.numeric()))
+
+	}
 	if("data.frame" %in% class(vec)){
 		for (i in 1:length(vec)) {
-			attr(vec[[i]],"table") <- table(vec[[i]],useNA = 'always')
+			tab <- table(vec[[i]],useNA = 'always')
+			attr(vec[[i]],"table") <- ll(tab,len=len)
 		}
 	}else{
-		attr(vec,"table") <- table(vec,useNA = 'always')
+		tab <- table(vec,useNA = 'always')
+		attr(vec,"table") <- ll(tab,len=len)
 	}
 	return(vec)
 }
+
--- inst/Rtest/testpackage.R
+++ inst/Rtest/testpackage.R
@@ -4,10 +4,10 @@ library(devtools)
 library(roxygen2)
 	roxygenise(clean = T)
 	document()
-}
+
 ##記得去github存檔
 ##install
-{
+
 library(devtools)
 library(YCfun)
 detach("package:YCfun", unload=TRUE)
